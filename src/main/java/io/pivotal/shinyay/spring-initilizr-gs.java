e930c22e20aff39976dd62b3dbb68e24d93187f5
shinyay <shinya.com@gmail.com>
2019-10-30 23:18:40 +0900

    Initial Commit

---
+++ src/main/java/io/pivotal/shinyay/spring-initilizr-gs.java
@@ -0,0 +1,13 @@
+package io.pivotal.shinyay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class spring-initilizr-gs {
+
+	public static void main(String[] args) {
+		SpringApplication.run(spring-initilizr-gs.class, args);
+	}
+
+}
