04fa1988a1a36e14e08c54b85b9cef3f127e10e3
shinyay <shinya.com@gmail.com>
2019-10-31 00:01:25 +0900

    Add WebJar for JQuery and BootStrap

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.webjars:bootstrap:4.3.1")
+	implementation("org.webjars:jquery:3.4.1")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
