c06675cd6d77dddd1f678f9ed8c9311e17377911
shinyay <shinya.com@gmail.com>
2019-10-30 23:26:09 +0900

    Modify reset project contents

--- initilizr.fish
+++ initilizr.fish
@@ -1,22 +1,20 @@
 #!/usr/bin/env fish
 
-set -g BOOT_VERSION 2.1.4.RELEASE
 set -g GROUP_ID io.pivotal.shinyay
-set -g ARTIFACT_ID sample
-set -g NAME sample
+set -g ARTIFACT_ID spring-initializr
+set -g NAME $ARTIFACT_ID
 set -g DESCRIPTION "Sample project for Spring Boot"
-set -g PACKAGE $GROUP_IS
-set -g DEPENDENCY web,actuator
+set -g PACKAGE $GROUP_ID
+set -g DEPENDENCY web,actuator,thymeleaf
 
 curl https://start.spring.io/starter.zip --create-dirs -o ./spring/spring.zip \
 	-d type=gradle-project \
 	-d language=kotlin \
-	-d bootVersion=2.1.4.RELEASE \
-	-d groupId=io.pivotal.shinyay \
-	-d artifactId=sample \
-	-d name=spring-app \
-	-d description="Sample project for Spring Boot" \
-	-d packageName=io.pivotal.shinyay \
+	-d groupId=$GROUP_ID \
+	-d artifactId=$ARTIFACT_ID \
+	-d name=$NAME \
+	-d description=$DESCRIPTION \
+	-d packageName=$PACKAGE \
 	-d packaging=jar \
-	-d javaVersion=1.11 \
+	-d javaVersion=1.8 \
 	-d dependencies=$DEPENDENCY
