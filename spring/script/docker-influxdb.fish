6d1af33c049ba2b56cba4693f1a657cef66f734d
shinyay <shinya.com@gmail.com>
2019-11-15 07:52:37 +0900

    Modify to add container instance name

--- spring/script/docker-influxdb.fish
+++ spring/script/docker-influxdb.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
-docker run --rm -p 8086:8086 -v /tmp:/var/lib/influxdb influxdb
+docker run --rm -d \
+--name=influxdb \
+-p 8086:8086 \
+-v /tmp:/var/lib/influxdb \
+influxdb
