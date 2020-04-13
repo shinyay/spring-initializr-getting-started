ad2ea1f6484f518c6fbba741bf63201e74cf3be7
shinyay <shinya.com@gmail.com>
2020-04-14 08:19:55 +0900

    Modify gradle configuration for Boot version and Java version

--- initilizr.fish
+++ initilizr.fish
@@ -1,13 +1,15 @@
 #!/usr/bin/env fish
+## https://docs.spring.io/initializr/docs/current/reference/html/#initializr-documentation
 
-set -g GROUP_ID io.pivotal.shinyay
-set -g ARTIFACT_ID spring-initializr
+set -g GROUP_ID com.google.shinyay
+set -g ARTIFACT_ID spring-boot-demo
 set -g NAME $ARTIFACT_ID
 set -g DESCRIPTION "Sample project for Spring Boot"
 set -g PACKAGE $GROUP_ID
 set -g DEPENDENCY web,actuator,thymeleaf
 
 curl https://start.spring.io/starter.zip --create-dirs -o ./spring/spring.zip \
+        -d bootVersion=2.2.6 \
 	-d type=gradle-project \
 	-d language=kotlin \
 	-d groupId=$GROUP_ID \
@@ -16,5 +18,5 @@ curl https://start.spring.io/starter.zip --create-dirs -o ./spring/spring.zip \
 	-d description=$DESCRIPTION \
 	-d packageName=$PACKAGE \
 	-d packaging=jar \
-	-d javaVersion=1.8 \
+	-d javaVersion=11 \
 	-d dependencies=$DEPENDENCY
